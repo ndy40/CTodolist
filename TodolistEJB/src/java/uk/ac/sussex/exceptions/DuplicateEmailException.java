9bc3c0ea840b59231d623b573538ea9c0dd63c97
ne51 <ne51@INF03900206.ad.susx.ac.uk>
2013-03-27 15:30:01 +0000

    DuplicateException added

---
+++ TodolistEJB/src/java/uk/ac/sussex/exceptions/DuplicateEmailException.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uk.ac.sussex.exceptions;
+
+/**
+ *
+ * @author ne51
+ */
+public class DuplicateEmailException extends Exception{
+
+    public DuplicateEmailException(String message) {
+        super(message);
+    }
+    
+    
+}
